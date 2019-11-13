a1ba5e321001485b1450ac5a9a3476417e69ee40
pailin.h3 <jinjuta.l@st.econ.tu.ac.th>
2019-11-13 15:18:57 +0700

    push pai korn

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="container">
-    <main-page></main-page>
+    <minesweeper></minesweeper>
   </div>
 </template>
 
 <script>
-import MainPage from './components/MainPage.vue'
+// import MainPage from './components/MainPage.vue'
+import Minesweeper from './components/game/Minesweeper.vue'
 
 export default {
   name: 'app',
   components: {
-    MainPage
+    // MainPage,
+    Minesweeper
   }
 }
 </script>
--- src/components/game/Minesweeper.vue
+++ src/components/game/Minesweeper.vue
@@ -1,14 +1,13 @@
 <template>
-    <div class="card-content">
-        <b-flex>
+    <div>
+        <b-field>
             <b-button @click="easy">easy</b-button>
             <b-button @click="normal">normal</b-button>
             <b-button @click="hard">hard</b-button>
-        </b-flex>
-        <p>level : {{level}}</p>
-        <b-flex>
-            <b-button id="state" @click="vanich">x</b-button>
-        </b-flex>
+        </b-field>
+        <div class="columns">
+            <div class="column is-4" :key="poom.id" v-for="poom in pooms" id="state" @click="step" >{{ poom.value }}</div>
+        </div> 
     </div>
 </template>
 
@@ -16,32 +15,52 @@
 export default {
     data() {
         return {
-            level: 1
+            lev: "",
+            level: [
+                {id:1,width:8,hight:8,bomb:20},
+                {id:2,width:10,hight:10,bomb:30},
+                {id:3,width:12,hight:12,bomb:40}
+            ],
+            pooms: [
+                {id: 1 , value: "x"},
+                {id: 2 , value: "2"},
+                {id: 3 , value: "1"},
+                {id: 4 , value: "2"},
+                {id: 5 , value: "x"},
+                {id: 6 , value: "1"},
+                {id: 7 , value: "1"},
+                {id: 8 , value: "1"},
+                {id: 9 , value: "1"},
+            ]
         }
     },
     methods: {
             easy() {
-                this.level=1
+                this.level.id=1
+                this.lev="easy"
                 this.$buefy.notification.open('Level 1')
             },
             normal() {
-                this.level=2
+                this.level.id=2
+                this.lev="normal"
                 this.$buefy.notification.open('Level 2')
             },
             hard() {
-                this.level=3
+                this.level.id=3
+                this.lev="hard"
                 this.$buefy.notification.open('Level 3')
             },
-            vanich() {
+            step() {
                 
-            }
+            },
+
         }
 }
 </script>
 
 <style scoped>
     #state {
-        color: darkgrey;
+        color: rgb(173, 0, 0);
         background-color: darkgray;
     }
 </style>
